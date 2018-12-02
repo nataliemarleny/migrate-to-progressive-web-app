a0bae0ab321b396b3bfc64215f395222f26c2f79
Natalie Marleny <nataliemarleny@gmail.com>
2018-12-02 16:42:09 +0000

    Update to service worker

--- work/sw.js
+++ work/sw.js
@@ -44,13 +44,6 @@ self.addEventListener('fetch', function(event) {
   }
 });
 
-event.respondWith(
-  caches.match(event.request).then(function(response) {
-    return response || fetch(event.request);
-  })
-);
-});
-
 self.addEventListener('push', function(event) {
   event.waitUntil(
     self.registration.showNotification('Got Push?', {
