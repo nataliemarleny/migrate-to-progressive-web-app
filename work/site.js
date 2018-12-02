3d173656490efd4057dc85390bc8a498a84ec733
Natalie Marleny <nataliemarleny@gmail.com>
2018-12-02 14:47:12 +0000

    Add push notifications and install web-push

--- work/site.js
+++ work/site.js
@@ -14,6 +14,29 @@
 
 /** Intentionally empty */
 
+const publicKey = 'BNHY0NPTArFg1-MN7oIiKGI3j8LOyTPPPEfrOLQ7CynKrr3w5BTZCq7vNjHL2yNyHPO_rG2XgMkh6ZdIpFSjhFE';
+
 navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration) {
   console.log('Excellent, registered with scope: ', registration.scope);
 });``
+
+navigator.serviceWorker && navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+  serviceWorkerRegistration.pushManager.getSubscription()
+    .then(function(subscription) {
+      if (subscription) {
+        console.info('Got existing', subscription);
+        window.subscription = subscription;
+        return;  // got one, yay
+      }
+
+      const applicationServerKey = urlB64ToUint8Array(publicKey);
+      serviceWorkerRegistration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey,
+      })
+        .then(function(subscription) {
+          console.info('Newly subscribed to push!', subscription);
+          window.subscription = subscription;
+        });
+    });
+});
---
+++ work/sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('push', function(event) {
+  event.waitUntil(
+    self.registration.showNotification('Got Push?', {
+      body: 'Push Message received'
+   }));
+});
